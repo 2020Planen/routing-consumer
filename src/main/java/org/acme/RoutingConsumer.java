3af559781c4dd584368709c06a7622bd429ad3db
rlj <rlj@convergens.dk>
2020-01-30 15:34:36 +0100

    works with new dependencies (1.2.12)

--- src/main/java/org/acme/RoutingConsumer.java
+++ src/main/java/org/acme/RoutingConsumer.java
@@ -9,13 +9,9 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
-import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import javax.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -27,27 +23,31 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 @ApplicationScoped
 public class RoutingConsumer {
 
-    private String url = "http://cis-x.convergens.dk:5984/routingslips/_design/RoutingSlip/_view/by_producerReference?key=\"";
+    private String url = "http://cis-x.convergens.dk:5984/routingslips/_design/RoutingSlip/_view/by_producerReference?key=";
     Gson gson = new Gson();
 
     @Incoming("routing")
     public void consume(String msg) throws IOException, Exception {
-        ConfigJsonObject cjObj = new ConfigJsonObject("routing");
+        PlayGround pg = new PlayGround("routing", msg);
 
-        Map incomingJson = gson.fromJson(msg, Map.class);
-        String producerRef = (String) incomingJson.get("producerReference");
-
-        JsonObject conditionsJson = getConditions(producerRef);
-
-        JsonElement conditionsElm = conditionsJson.getAsJsonArray("rows").get(0).getAsJsonObject().get("value").deepCopy();
-        incomingJson.put("conditions", conditionsElm);
         
-        java.lang.reflect.Type gsonType = new TypeToken<HashMap>() {
-        }.getType();
-        String enrichedMsg = gson.toJson(incomingJson, gsonType);
+        String producerRef = pg.getJsonMessage().get("producerReference").toString();
+        String conditionsStr = getJsonData(url + producerRef + "");
+        JsonObject conditionsJson = new JsonParser().parse(conditionsStr).getAsJsonObject();
+
         
-        cjObj.convertJsonToEntity(enrichedMsg);
-        cjObj.director();
+        JsonElement conditionsElm = conditionsJson
+                .getAsJsonArray("rows")
+                .get(0)
+                .getAsJsonObject()
+                .get("value")
+                .deepCopy();
+
+        pg.appendJson("conditions", conditionsElm);
+        
+        
+        pg.routingSlipDirector();
+
     }
 
     public String getJsonData(String url) throws MalformedURLException, IOException {
@@ -59,9 +59,4 @@ public class RoutingConsumer {
         return response;
     }
 
-    private JsonObject getConditions(String producerRef) throws IOException, JsonSyntaxException {
-        String conditionsStr = getJsonData(url + producerRef + "\"");
-        JsonObject conditionsJson = new JsonParser().parse(conditionsStr).getAsJsonObject();
-        return conditionsJson;
-    }
 }
